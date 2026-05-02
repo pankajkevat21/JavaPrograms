9ba758d7a65e4ab337fb340e6779ab08fff41f6b
pankajkevat21 <pankajkevat21@gmail.com>
2026-05-02 16:13:51 +0530

    And.java file modified

--- And.java
+++ And.java
@@ -2,6 +2,6 @@ public class And {
     public static void main(String[] args) {
         int a = 13& 25;
         System.out.println(a);
-        System.out.println("again changing in git commit");
+        System.out.println("again changing in git commit and push on to the github");
     }
 }
