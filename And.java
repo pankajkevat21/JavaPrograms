ebcd9ea991674d24e70cf773efdfbac0e33abf88
pankajkevat21 <pankajkevat21@gmail.com>
2026-05-02 16:06:42 +0530

    finally added all changes

--- And.java
+++ And.java
@@ -2,5 +2,6 @@ public class And {
     public static void main(String[] args) {
         int a = 13& 25;
         System.out.println(a);
+        System.out.println("again changing in git commit");
     }
 }
