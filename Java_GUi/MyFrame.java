6dcefd6a8a1a8cf793c38770ce4cebe3422831bd
pankajkevat21 <pankajkevat21@gmail.com>
2026-05-05 16:55:56 +0530

    Extra space delete kiya hai

--- Java_GUi/MyFrame.java
+++ Java_GUi/MyFrame.java
@@ -5,8 +5,6 @@ import javax.swing.*;
 public class MyFrame {
 
     private static JFrame frame;   // static (global access)
-
-    // private constructor (object create nahi kar paoge bahar se)
     private MyFrame() {}
 
     public static JFrame getFrame() {
