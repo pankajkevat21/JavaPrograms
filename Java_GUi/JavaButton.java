4dc93d7bb9e3447928804c11168c72bea589a436
pankajkevat21 <pankajkevat21@gmail.com>
2026-05-05 16:22:40 +0530

    image on button

--- Java_GUi/JavaButton.java
+++ Java_GUi/JavaButton.java
@@ -6,8 +6,8 @@ public class JavaButton  {
     public static void main(String[] args) {
 
         JFrame frame = MyFrame.getFrame();
-        JButton b1 = new JButton("Click Me");
-        b1.setBounds(120, 110, 120, 40);
+        JButton b1 = new JButton(new ImageIcon("C:\\Users\\panka\\IdeaProjects\\Pankajapplication\\Screenshot 2026-04-29 200536.png"));
+        b1.setBounds(0, 0, 1207, 635);
 
         frame.add(b1);
     }
