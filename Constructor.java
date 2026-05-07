0aa7eb99368736cefa7a87918ea3f96ebeb39738
pankajkevat21 <pankajkevat21@gmail.com>
2026-05-07 19:57:32 +0530

    Constructor added

---
+++ Constructor.java
@@ -0,0 +1,27 @@
+class Base1{
+    Base1(){
+        System.out.println("I am base1 class defualt constructor");
+    }
+    Base1(int k){
+        System.out.println("i am single parameter constructor "+k);
+    }
+    Base1(int k, int l){
+        System.out.println("i am double parametrerized constructor and sum:"+(k+l));
+    }
+}
+class  Derived {
+    Derived(){
+        System.out.println("i am derived class default constructor");
+    }
+    Derived(int k){
+        System.out.println("i am derived class Single parameterized constructor"+k);
+    }
+    Derived(int m, int n){
+        System.out.println("i am derived class double parameterized constructor and sum:"+(m+n));
+    }
+}
+public class Constructor {
+    public static void main(String[] args) {
+        Derived d1 = new Derived();
+    }
+}
