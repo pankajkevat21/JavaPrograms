1e1ef716f61eead13aa6674f8df810dc69081932
pankajkevat21 <pankajkevat21@gmail.com>
2026-05-12 19:32:49 +0530

    wrapper added  with method

---
+++ Java_Advanced/Wrapper_object_method.java
@@ -0,0 +1,12 @@
+package Java_Advanced;
+
+public class Wrapper_object_method {
+    public static void main(String[] args) {
+        Integer myInt = 5;
+        Double myDouble = 5.99;
+        Character myChar = 'A';
+        System.out.println(myInt.intValue());
+        System.out.println(myDouble.doubleValue());
+        System.out.println(myChar.charValue());
+    }
+}
