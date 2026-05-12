3387208b4adf064fdadc29dad7c62309558b5e80
pankajkevat21 <pankajkevat21@gmail.com>
2026-05-12 19:33:59 +0530

    wrapper int to string

---
+++ Java_Advanced/Wrapper_String.java
@@ -0,0 +1,10 @@
+package Java_Advanced;
+
+public class Wrapper_String {
+        public static void main(String[] args) {
+            Integer myInt = 100;
+            String myString = myInt.toString();
+            System.out.println(myString.length());
+        }
+    }
+
