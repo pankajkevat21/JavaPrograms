997a501f297c38558c6d7a187a1e9243b72725f0
pankajkevat21 <pankajkevat21@gmail.com>
2026-05-12 22:27:47 +0530

    double data type added on the ArrayList

--- TestingPrograms/Solution.java
+++ TestingPrograms/Solution.java
@@ -3,13 +3,21 @@ import  java.util.ArrayList;
 class Solution{
     public static void main(String[] args) {
         ArrayList<Integer> arr = new ArrayList<>();
+        ArrayList<Double> arr2 = new ArrayList<>();
         arr.add(50);
         arr.add(10);
         arr.add(20);
         arr.add(30);
         arr.add(40);
-        arr.add(60);
+        arr2.add(60.1);
+        arr2.add(10.9);
+        arr2.add(20.7);
+        arr2.add(30.6);
+        arr2.add(40.5);
+        arr2.add(60.5);
         System.out.println(arr);
+        System.out.println(arr2);
+
 
      }
 
