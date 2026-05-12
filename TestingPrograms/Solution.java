e279bd5b20ce8e5c07627bd7342590c9a8b387e5
pankajkevat21 <pankajkevat21@gmail.com>
2026-05-12 22:26:09 +0530

    array print

--- TestingPrograms/Solution.java
+++ TestingPrograms/Solution.java
@@ -1,17 +1,16 @@
-
 package  TestingPrograms;
+import  java.util.ArrayList;
 class Solution{
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7};
-        for (int st = 0; st < arr.length; st++) {
-            for (int end = st; end <arr.length ; end++) {
-                for (int i = st; i <end ; i++) {
-                    System.out.print(arr[i]);
-                }
-                System.out.print(" ");
-            }
-            System.out.println();
-        }
-    }
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(50);
+        arr.add(10);
+        arr.add(20);
+        arr.add(30);
+        arr.add(40);
+        arr.add(60);
+        System.out.println(arr);
+
+     }
 
 }
