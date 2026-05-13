f0952fe0b650ffbbc480a4f7dcb3892d9a8cff29
pankajkevat21 <pankajkevat21@gmail.com>
2026-05-13 22:18:05 +0530

    new content

--- TestingPrograms/Solution.java
+++ TestingPrograms/Solution.java
@@ -4,19 +4,11 @@ class Solution{
     public static void main(String[] args) {
         ArrayList<Integer> arr = new ArrayList<>();
         ArrayList<Double> arr2 = new ArrayList<>();
-        arr.add(50);
-        arr.add(10);
-        arr.add(20);
-        arr.add(30);
-        arr.add(40);
-        arr2.add(60.1);
-        arr2.add(10.9);
-        arr2.add(20.7);
-        arr2.add(30.6);
-        arr2.add(40.5);
-        arr2.add(60.5);
-        System.out.println(arr);
-        System.out.println(arr2);
+       Integer myint = 8;
+        System.out.println(myint.intValue());
+        Integer my = 1000;
+        String mystr = my.toString();
+        System.out.println(mystr.length());
 
 
      }
