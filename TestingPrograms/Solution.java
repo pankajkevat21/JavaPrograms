38dcd2d603e4dd4790efb5567f79ab22ff771f39
pankajkevat21 <pankajkevat21@gmail.com>
2026-05-14 18:50:46 +0530

    generic class with number

--- TestingPrograms/Solution.java
+++ TestingPrograms/Solution.java
@@ -1,16 +1,27 @@
 package TestingPrograms;
-class Solution{
-    public static <T> void printArray(T[] are)
-    {
-        for (T item: are)
-        {
-            System.out.println(item);
-        }
+class Stats<T extends Number>
+{
+    T[]   nums;
+    Stats(T[] nums){
+        this.nums = nums;
     }
+    double average() {
+        double sum = 0;
+        for (T num : nums) {
+            sum += num.doubleValue();
+        }
+        return sum / nums.length;
+    }
+}
+public class Solution{
     public static void main(String[] args) {
-        String[] names = {"pankaj", "rohit"};
-        Integer[] numbers = {10,20,30,40,50};
-        printArray(names);
-        printArray(numbers);
+        Integer[] intNums = {10, 20, 30, 40};
+        Stats<Integer> intStats = new Stats<>(intNums);
+        System.out.println("Integer average: " + intStats.average());
+
+        // Use with Double
+        Double[] doubleNums = {1.5, 2.5, 3.5};
+        Stats<Double> doubleStats = new Stats<>(doubleNums);
+        System.out.println("Double average: " + doubleStats.average());
     }
 }
