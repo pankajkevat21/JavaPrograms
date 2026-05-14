ff4af7625f00e71e43e4e66b2637d62919c60f6c
pankajkevat21 <pankajkevat21@gmail.com>
2026-05-14 18:23:23 +0530

    generics workded

--- TestingPrograms/Solution.java
+++ TestingPrograms/Solution.java
@@ -1,22 +1,20 @@
 package TestingPrograms;
-class Box<T>
-{
-    T  value;// T is a placeholder for any data type
-    void set(T value){
-        this.value = value;
-}
-T get() {
-        return value;
-}
+class Box<t1> {
+    t1 value;
 
-}
-class Solution{
-    public static void main(String[] args) {
-        Box<String> bx = new Box<>();
-        bx.set("pankaj");
-        System.out.println("value:"+bx.get());
-        Box<Integer> it = new Box<>();
-        it.set(50);
-        System.out.println("value:"+it.get());
+    void set(t1 value) {
+        this.value = value;
+    }
+
+    t1 get() {
+        return value;
     }
 }
+    class Solution {
+
+        public static void main(String[] args) {
+       Box<Integer> bx = new Box<>();
+       bx.set(12);
+            System.out.println(bx.get() );
+        }
+    }
