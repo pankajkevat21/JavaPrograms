59ca7afb8447ccb8384a0fc25fd1e5ecb19e4f3a
pankajkevat21 <pankajkevat21@gmail.com>
2026-05-14 18:40:18 +0530

    added

--- TestingPrograms/Solution.java
+++ TestingPrograms/Solution.java
@@ -1,20 +1,16 @@
 package TestingPrograms;
-class Box<t1> {
-    t1 value;
-
-    void set(t1 value) {
-        this.value = value;
-    }
-
-    t1 get() {
-        return value;
-    }
-}
-    class Solution {
-
-        public static void main(String[] args) {
-       Box<Integer> bx = new Box<>();
-       bx.set(12);
-            System.out.println(bx.get() );
+class Solution{
+    public static <T> void printArray(T[] are)
+    {
+        for (T item: are)
+        {
+            System.out.println(item);
         }
     }
+    public static void main(String[] args) {
+        String[] names = {"pankaj", "rohit"};
+        Integer[] numbers = {10,20,30,40,50};
+        printArray(names);
+        printArray(numbers);
+    }
+}
