02308c1d71125f637a2e962fa5c79fd2c848f9c7
pankajkevat21 <pankajkevat21@gmail.com>
2026-05-15 21:44:49 +0530

    small 1

--- smallpro/HelloWorld2.java
+++ smallpro/HelloWorld2.java
@@ -1,7 +1,9 @@
-package smallpro.smallpro;
+// HelloWorld2.java
+package smallpro;
 
 public class HelloWorld2 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int a = 10, b = 20;
+        System.out.println("Sum = " + (a + b));
     }
 }
