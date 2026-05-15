9bbc15a10675c108247da07da1a9eabf40957627
pankajkevat21 <pankajkevat21@gmail.com>
2026-05-15 21:46:35 +0530

    small 6

--- smallpro/HelloWorld6.java
+++ smallpro/HelloWorld6.java
@@ -1,7 +1,21 @@
-package smallpro.smallpro;
+// HelloWorld6.java
+package smallpro;
 
 public class HelloWorld6 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int n = 11;
+        boolean prime = true;
+
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                prime = false;
+                break;
+            }
+        }
+
+        if (prime)
+            System.out.println("Prime");
+        else
+            System.out.println("Not Prime");
     }
 }
