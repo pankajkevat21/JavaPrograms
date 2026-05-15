19fc971fb8ff0e09260a111f1f611fc7a6acd7cd
pankajkevat21 <pankajkevat21@gmail.com>
2026-05-15 21:47:28 +0530

    small 8

--- smallpro/HelloWorld8.java
+++ smallpro/HelloWorld8.java
@@ -1,7 +1,16 @@
-package smallpro.smallpro;
+// HelloWorld8.java
+package smallpro;
 
 public class HelloWorld8 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int num = 1234;
+        int rev = 0;
+
+        while (num != 0) {
+            rev = rev * 10 + num % 10;
+            num /= 10;
+        }
+
+        System.out.println("Reverse = " + rev);
     }
 }
