9feb09560505cc1cd4df76be10590fdee65e0e40
pankajkevat21 <pankajkevat21@gmail.com>
2026-05-15 21:45:16 +0530

    small 3

--- smallpro/HelloWorld3.java
+++ smallpro/HelloWorld3.java
@@ -1,7 +1,13 @@
-package smallpro.smallpro;
+// HelloWorld3.java
+package smallpro;
 
 public class HelloWorld3 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int num = 7;
+
+        if (num % 2 == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
     }
 }
