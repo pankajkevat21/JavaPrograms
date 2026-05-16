031610903c238454dc7fc6e66c707912fbf537da
pankajkevat21 <pankajkevat21@gmail.com>
2026-05-16 21:58:29 +0530

    small 10

--- smallpro/HelloWorld.java
+++ smallpro/HelloWorld.java
@@ -3,5 +3,13 @@ package smallpro;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
