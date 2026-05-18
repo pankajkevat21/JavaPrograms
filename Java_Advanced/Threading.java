50adc1dabed41a8cded5e57e5317463e1cb34dff
pankajkevat21 <pankajkevat21@gmail.com>
2026-05-18 17:39:10 +0530

    runnable interface implements successfully

--- Java_Advanced/Threading.java
+++ Java_Advanced/Threading.java
@@ -1,5 +1,5 @@
 package Java_Advanced;
-
+/*
 public class Threading extends  Thread{
     public void run(){
         System.out.println("this code is inside of thread");
@@ -10,4 +10,15 @@ public class Threading extends  Thread{
         System.out.println("This code is outside of thread");
     }
 
+}
+*/
+public class Threading implements Runnable{
+    public void run(){
+        System.out.println("this code is inside of thread");
+    }
+    public static void main(String[] args) {
+        Threading th = new Threading();
+       Thread obj = new Thread(th);
+       obj.start();
+    }
 }
