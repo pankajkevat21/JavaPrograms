77f0e1688f873f7dfd9997ee1ed0c0906b6eb143
pankajkevat21 <pankajkevat21@gmail.com>
2026-05-21 19:24:07 +0530

    Graph created

--- Graphs/Create_Graph.java
+++ Graphs/Create_Graph.java
@@ -6,15 +6,13 @@ class Graphs{
     Graphs(int V){
         this.V =V;
         adjList = new ArrayList<>();
-        for (int i = 0; i <V ; i++) {
+        for (int i = 0; i <V ; i++)
             {
                 adjList.add(new ArrayList<>());
             }
-
         }
-    }
     // Edge add karne ka method
-    void addEdge(char src, char dest) {
+    void addEdge(int src, int dest) {
         adjList.get(src).add(dest);
         adjList.get(dest).add(src);
     }
@@ -25,32 +23,18 @@ class Graphs{
             for (Integer node : adjList.get(i)) {
                 System.out.print(node + " ");
             }
-
             System.out.println();
         }
     }
 }
 public class Create_Graph {
     public static void main(String[] args) {
-        System.out.println("I am creating a new branch and working in together");
         Graphs g = new Graphs(8);
-        g.addEdge('A', 'B');
-        g.addEdge('A', 'C');
-        g.addEdge('B', 'D');
-        g.addEdge('B', 'E');
-        g.addEdge('C', 'F');
-        g.addEdge('C', 'E');
-        g.addEdge('D', 'G');
-        g.addEdge('I', 'G');
-        g.addEdge('I', 'H');
-        g.addEdge('H', 'F');
-        g.addEdge('G', 'E');
-        g.addEdge('E', 'H');
-
-
-
-
-       
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 3);
+        g.addEdge(1, 4);
+        g.addEdge(2, 5);
         g.printGraph();
     }
 }
