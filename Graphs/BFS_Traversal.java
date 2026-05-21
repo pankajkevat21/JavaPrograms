12cf1ab2c7a544c39f6a5dd3f5bb6e469488d7c1
pankajkevat21 <pankajkevat21@gmail.com>
2026-05-21 19:26:16 +0530

    Graph created and successfully traversaled in graph using BFS traversal method

--- Graphs/BFS_Traversal.java
+++ Graphs/BFS_Traversal.java
@@ -1,25 +1,32 @@
 package Graphs;
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
 class Graph {
+
     int V;
+
     ArrayList<ArrayList<Integer>> adjList;
 
-    Graph(int V) {
+    Graph(int V)
+    {
         this.V = V;
 
         adjList = new ArrayList<>();
 
-        for (int i = 0; i < V; i++) {
+        for(int i = 0; i < V; i++)
+        {
             adjList.add(new ArrayList<>());
         }
     }
 
-    void addEdge(int src, int dest) {
+    void addEdge(int src, int dest)
+    {
         adjList.get(src).add(dest);
-        adjList.get(dest).add(src);//Remove this for directed graph
+
+        adjList.get(dest).add(src);
     }
 
     void BFS(int start)
@@ -29,13 +36,14 @@ class Graph {
         Queue<Integer> q = new LinkedList<>();
 
         visited[start] = true;
+
         q.add(start);
 
         while(!q.isEmpty())
         {
             int curr = q.poll();
 
-            System.out.print(curr + "->");
+            System.out.print(curr + " ");
 
             for(Integer neighbour : adjList.get(curr))
             {
@@ -44,31 +52,49 @@ class Graph {
                     visited[neighbour] = true;
 
                     q.add(neighbour);
-
-                    System.out.print(neighbour + " ");
                 }
             }
+        }
+    }
 
-            System.out.println();
+    void DFS(int curr, boolean[] visited)
+    {
+        visited[curr] = true;
+
+        System.out.print(curr + " ");
+
+        for(Integer neighbour : adjList.get(curr))
+        {
+            if(!visited[neighbour])
+            {
+                DFS(neighbour, visited);
+            }
         }
     }
 }
 
 public class BFS_Traversal {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args)
+    {
         Graph g = new Graph(8);
 
-        g.addEdge(0, 1);
-        g.addEdge(0, 2);
-        g.addEdge(1, 3);
-        g.addEdge(1, 4);
-        g.addEdge(2, 5);
+        g.addEdge(0,1);
+        g.addEdge(0,2);
+        g.addEdge(1,3);
+        g.addEdge(1,4);
+        g.addEdge(2,5);
 
         System.out.println("BFS Traversal:");
 
         g.BFS(0);
 
+        System.out.println();
+
+        System.out.println("DFS Traversal:");
+
+        boolean[] visited = new boolean[g.V];
+
+        g.DFS(0, visited);
     }
 }
