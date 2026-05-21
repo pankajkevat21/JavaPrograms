f9f8a2ff947c93663831696116571a226c99752d
pankajkevat21 <pankajkevat21@gmail.com>
2026-05-21 19:24:39 +0530

    Graph created and successfully traversaled in graph

---
+++ Graphs/BFS_Traversal.java
@@ -0,0 +1,74 @@
+package Graphs;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Graph {
+    int V;
+    ArrayList<ArrayList<Integer>> adjList;
+
+    Graph(int V) {
+        this.V = V;
+
+        adjList = new ArrayList<>();
+
+        for (int i = 0; i < V; i++) {
+            adjList.add(new ArrayList<>());
+        }
+    }
+
+    void addEdge(int src, int dest) {
+        adjList.get(src).add(dest);
+        adjList.get(dest).add(src);//Remove this for directed graph
+    }
+
+    void BFS(int start)
+    {
+        boolean[] visited = new boolean[V];
+
+        Queue<Integer> q = new LinkedList<>();
+
+        visited[start] = true;
+        q.add(start);
+
+        while(!q.isEmpty())
+        {
+            int curr = q.poll();
+
+            System.out.print(curr + "->");
+
+            for(Integer neighbour : adjList.get(curr))
+            {
+                if(!visited[neighbour])
+                {
+                    visited[neighbour] = true;
+
+                    q.add(neighbour);
+
+                    System.out.print(neighbour + " ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
+
+public class BFS_Traversal {
+
+    public static void main(String[] args) {
+
+        Graph g = new Graph(8);
+
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 3);
+        g.addEdge(1, 4);
+        g.addEdge(2, 5);
+
+        System.out.println("BFS Traversal:");
+
+        g.BFS(0);
+
+    }
+}
