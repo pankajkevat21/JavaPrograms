f7ceeeafd6c7a6276aa4a5455bd382d9406130c1
pankajkevat21 <pankajkevat21@gmail.com>
2026-05-22 19:06:41 +0530

    new graph create

--- Graphs/new_Graph.java
+++ Graphs/new_Graph.java
@@ -2,30 +2,42 @@ package Graphs;
 
 import java.util.ArrayList;
 
-class graf{
+public class new_Graph {
     int V;
     ArrayList<ArrayList<Integer>> adjList;
-    graf(int V){
+
+    new_Graph(int V) {
         this.V = V;
         adjList = new ArrayList<>();
         for (int i = 0; i < V; i++) {
             adjList.add(new ArrayList<>());
         }
     }
-void addEdge(int src, int dest)
-        {
-            adjList.get(src).add(dest);
-            adjList.get(dest).add(src);
-        }
-        void printGraph(){
-            for (int i = 0; i < V; i++) {
-                System.out.print(i + "->");
-                for (Integer neigbour : adjList.get(i)) {
-                    System.out.print(neigbour+" ");
-                }
-                System.out.println();
+
+    void addEdge(int src, int dest) {
+        adjList.get(src).add(dest);
+        adjList.get(dest).add(src);
+    }
+
+    void printGraph() {
+        for (int i = 0; i < V; i++) {
+            System.out.print(i + "->");
+            for (Integer neigbour : adjList.get(i)) {
+                System.out.print(neigbour + " ");
             }
+            System.out.println();
         }
-}
-public class new_Graph {
+    }
+
+    public static void main(String[] args) {
+
+        new_Graph g = new new_Graph(5);
+
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 3);
+        g.addEdge(2, 4);
+
+        g.printGraph();
+    }
 }
