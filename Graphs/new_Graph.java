af171d0c8efa9709f7eb8eff521cd4301e7be34d
pankajkevat21 <pankajkevat21@gmail.com>
2026-05-22 19:01:25 +0530

    print graph

--- Graphs/new_Graph.java
+++ Graphs/new_Graph.java
@@ -11,8 +11,21 @@ class graf{
         for (int i = 0; i < V; i++) {
             adjList.add(new ArrayList<>());
         }
-
     }
+void addEdge(int src, int dest)
+        {
+            adjList.get(src).add(dest);
+            adjList.get(dest).add(src);
+        }
+        void printGraph(){
+            for (int i = 0; i < V; i++) {
+                System.out.print(i + "->");
+                for (Integer neigbour : adjList.get(i)) {
+                    System.out.print(neigbour+" ");
+                }
+                System.out.println();
+            }
+        }
 }
 public class new_Graph {
 }
