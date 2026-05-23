3a1b35f81492b1d9383f81f9c9a1370cf0935db2
pankajkevat21 <pankajkevat21@gmail.com>
2026-05-23 20:12:58 +0530

    Check leap year or not

---
+++ AcroQuestions/Q17LeapYear.java
@@ -0,0 +1,18 @@
+package AcroQuestions;
+
+import java.util.Scanner;
+public class Q17LeapYear {
+ public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Enter year: ");
+            int year = sc.nextInt();
+
+            if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+                System.out.println(year + " is a Leap Year");
+            } else {
+                System.out.println(year + " is NOT a Leap Year");
+            }
+        }
+    }
