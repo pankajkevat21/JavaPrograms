6238d07660bd257790fd3fcccdb29dc25e3bdbed
pankajkevat21 <pankajkevat21@gmail.com>
2026-05-24 22:56:05 +0530

    binary search without recursion

--- AcroQuestions/Q21BinarySearchNonRecursive.java
+++ AcroQuestions/Q21BinarySearchNonRecursive.java
@@ -1,4 +1,42 @@
 package AcroQuestions;
 
 public class Q21BinarySearchNonRecursive {
-}
+
+        static int binarySearch(int arr[], int target) {
+
+            int low = 0;
+            int high = arr.length - 1;
+
+            while (low <= high) {
+
+                int mid = (low + high) / 2;
+
+                if (arr[mid] == target) {
+                    return mid;
+                }
+
+                else if (target < arr[mid]) {
+                    high = mid - 1;
+                }
+
+                else {
+                    low = mid + 1;
+                }
+            }
+
+            return -1;
+        }
+
+        public static void main(String[] args) {
+
+            int arr[] = {10, 20, 30, 40, 50, 60, 70};
+            int target = 50;
+
+            int result = binarySearch(arr, target);
+
+            if (result != -1)
+                System.out.println("Element found at index: " + result);
+            else
+                System.out.println("Element not found");
+        }
+    }
