800b6b2f122794da8c5ae873b8f6d1c3d948dd8d
pankajkevat21 <pankajkevat21@gmail.com>
2026-05-24 23:00:17 +0530

    Array reverse using two pointer apporach

--- TestingPrograms/CheckEqual.java
+++ TestingPrograms/CheckEqual.java
@@ -1,11 +1,23 @@
 package TestingPrograms;
-
+import java.util.*;
 public class CheckEqual {
     public static void main(String[] args) {
-        Integer as = null;
-        System.out.println(as);
-        Integer bs= 200;
-        System.out.println((as==bs));
+        int[] arr = {1, 2, 3, 4, 5};
 
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start < end) {
+
+            // swap
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+
+        System.out.println(Arrays.toString(arr));
     }
 }
