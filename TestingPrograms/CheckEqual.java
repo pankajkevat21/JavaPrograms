b1d9e658114044bb48a07c410f0a32a6b57329f7
pankajkevat21 <pankajkevat21@gmail.com>
2026-05-24 22:58:23 +0530

    checking Equality

---
+++ TestingPrograms/CheckEqual.java
@@ -0,0 +1,11 @@
+package TestingPrograms;
+
+public class CheckEqual {
+    public static void main(String[] args) {
+        Integer as = null;
+        System.out.println(as);
+        Integer bs= 200;
+        System.out.println((as==bs));
+
+    }
+}
