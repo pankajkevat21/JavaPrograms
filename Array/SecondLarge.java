0178244bfd718a3a430f786ad0c3c9726fcb4ca4
pankajkevat21 <pankajkevat21@gmail.com>
2026-05-26 23:03:30 +0530

    secondlargest element in the array

--- Array/SecondLarge.java
+++ Array/SecondLarge.java
@@ -23,6 +23,7 @@ public class SecondLarge {
         }
         System.out.println("Second Largest = " + secondlargest);
         System.out.println("Second Largest = " + secondlargest);
+        System.out.println("Second Largest = " + secondlargest);
 
         System.out.println("Largest = " + largest);
         System.out.println("Second Largest = " + secondlargest);
