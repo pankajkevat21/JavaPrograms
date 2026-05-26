9482f6fad97d42cb919d7b97f86747d013fb669b
pankajkevat21 <pankajkevat21@gmail.com>
2026-05-26 23:05:09 +0530

    secondlargest element in the array

--- Array/SecondLarge.java
+++ Array/SecondLarge.java
@@ -31,6 +31,7 @@ public class SecondLarge {
         System.out.println("Second Largest = " + secondlargest);
         System.out.println("Second Largest = " + secondlargest);
         System.out.println("Second Largest = " + secondlargest);
+        System.out.println("Second Largest = " + secondlargest);
 
         System.out.println("Largest = " + largest);
         System.out.println("Second Largest = " + secondlargest);
