12099d241e8f699958b209fa7e54458e774f29f7
pankajkevat21 <pankajkevat21@gmail.com>
2026-05-27 20:27:15 +0530

    array reverse

---
+++ Array/Revers.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class Revers {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        int start =0;
+        int end = arr.length-1;
+        while (start<end){
+            int temp = arr[start];
+            arr[start]= arr[end];
+            arr[end] = temp;
+            start++; end--;
+        }
+        for (int x: arr){
+            System.out.print(x+ " ");
+        }
+    }
+}
