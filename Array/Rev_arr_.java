ee7137051bb5e47545f17fa4f7d6aea1d6fb3864
pankajkevat21 <pankajkevat21@gmail.com>
2026-05-27 22:08:11 +0530

    array reverse using new array

--- Array/Rev_arr_.java
+++ Array/Rev_arr_.java
@@ -2,6 +2,7 @@ package Array;
 
 public class Rev_arr_ {
     public static void main(String[] args) {
+
         int[] arr = {10, 20, 30, 40, 50};
         int rev[] = new int[arr.length];
         for(int i = 0; i < arr.length; i++) {
