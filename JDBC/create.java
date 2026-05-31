79eb1228e1dc7885d94f000d92933f2418b09bcf
pankajkevat21 <pankajkevat21@gmail.com>
2026-05-31 18:09:55 +0530

    data inserted using jdbc

---
+++ JDBC/create.java
@@ -0,0 +1,39 @@
+package JDBC;
+import java.sql.*;
+
+public class create {
+
+    public static void main(String[] args) {
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            String url = "jdbc:mysql://localhost:3306/pankajdb";
+            String userName = "root";
+            String password = "1234";
+
+            Connection con =
+                    DriverManager.getConnection(url,userName,password);
+
+            Statement stm = con.createStatement();
+
+            String query1 =
+                    "INSERT INTO student VALUES(107,'Pankaj')";
+
+            String query2 =
+                    "INSERT INTO student VALUES(108,'Sujeet')";
+
+            stm.executeUpdate(query1);
+            stm.executeUpdate(query2);
+
+            System.out.println("Data inserted");
+
+            con.close();
+
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
