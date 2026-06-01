a4f9aa81b1792998c664462b99aee351a8dd3d97
pankajkevat21 <pankajkevat21@gmail.com>
2026-06-01 22:57:11 +0530

    data deleted into jdbc

--- JDBC/create.java
+++ JDBC/create.java
@@ -38,6 +38,7 @@ public class create {
             System.out.println("Data inserted");
             System.out.println("Data inserted");
             System.out.println("Data inserted");
+            System.out.println("Data inserted");
 
             con.close();
 
