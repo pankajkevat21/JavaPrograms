b8a07bd9ff8c951885d36b9538a3def34bcab01d
Pankaj Kevat <pankajkevat21@gmail.com>
2026-06-20 22:33:55 +0530

    min and max value of this array

--- Array/ArrayMaxElement.java
+++ Array/ArrayMaxElement.java
@@ -4,12 +4,17 @@ class ArrayMaxElement {
     public static void main(String[] args) {
         int[] arr ={600,3,7,8,90};
         int max =arr[0];
+        int min = arr[0];
         for (int i = 0; i < arr.length; i++) {
             if(max<arr[i]){
                 max = arr[i];
             }
+            if(min>arr[i]){
+                min = arr[i];
+            }
 
         }
-        System.out.println(max);
+        System.out.println("min value of array :"+min);
+        System.out.println("max element of this array"+max);
     }
 }
