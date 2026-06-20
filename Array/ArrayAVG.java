8139b6405ad184bb07eabd677d1ef5d00df1ee32
Pankaj Kevat <pankajkevat21@gmail.com>
2026-06-20 22:25:18 +0530

    array avg

--- Array/ArrayAVG.java
+++ Array/ArrayAVG.java
@@ -1,4 +1,13 @@
 package Array;
 
 public class ArrayAVG {
+    public static void main(String[] args) {
+        int[] arr ={2,5,7,4,3};
+        float avg =0f;
+        for (int i = 0; i < arr.length; i++) {
+            avg+= arr[i];
+        }
+        avg = avg/arr.length;
+        System.out.println(avg);
+    }
 }
