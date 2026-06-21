9805dcf632b239a9591402298ac820aa0fffbc82
Pankaj Kevat <pankajkevat21@gmail.com>
2026-06-21 22:28:29 +0530

    smallest element index in array

--- Array/smallestElementIndex.java
+++ Array/smallestElementIndex.java
@@ -1,4 +1,17 @@
 package Array;
 
 public class smallestElementIndex {
+    public static void main(String[] args) {
+        int[] arr ={2,4,8,0,6,1};
+        int smallIdx = 0;
+        int minValue = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if(minValue>arr[i])
+            {
+                minValue = arr[i];
+                smallIdx = i;
+            }
+        }
+        System.out.println("Smallest element index:"+smallIdx);
+    }
 }
