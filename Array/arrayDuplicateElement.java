6372bc1c05f3f790e5bce4b3c3b89018d774028f
Pankaj Kevat <pankajkevat21@gmail.com>
2026-06-21 22:55:08 +0530

    duplicate element in array

---
+++ Array/arrayDuplicateElement.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class arrayDuplicateElement {
+    public static void main(String[] args) {
+        int[] arr = {2,4,6,7,7,8};
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.println(arr[i]);
+                    break;
+                }
+            }
+        }
+    }
+}
