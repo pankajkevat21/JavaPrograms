04dc6f736e7ea22f0b2c534b1c6ac9c6acbfb945
Pankaj Kevat <pankajkevat21@gmail.com>
2026-06-21 18:08:20 +0530

    first and last occurance in array

---
+++ Array/INIIOccurrance.java
@@ -0,0 +1,22 @@
+package Array;
+
+public class INIIOccurrance {
+    public static void main(String[] args) {
+        int arr[] = {23, 6, 23, 7, 12, 87, 23, 23};
+        int firstOccurance = -1;
+        int lastOccurance = -1;
+        int target =23;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]==target && firstOccurance==-1){
+                lastOccurance = i+1;
+                firstOccurance =i+1;
+            }
+            if(arr[i]==target){
+                lastOccurance =i+1;
+            }
+        }
+        System.out.println("First occurance:"+firstOccurance);
+        System.out.println("Last occurance:"+lastOccurance);
+    }
+}
+
