2cedfe31a7c20117723d8fbf30365571d7f5096c
Pankaj Kevat <pankajkevat21@gmail.com>
2026-06-21 17:47:10 +0530

    element frequency added

--- Array/Find_Element.java
+++ Array/Find_Element.java
@@ -2,12 +2,14 @@ package Array;
 
 public class Find_Element {
     public static void main(String[] args) {
-        int arr[] ={3,6,7,12,87,23,23};
+        int arr[] ={3,6,23,7,12,87,23,23};
         int target = 23;
+        int count =0;
         for (int i = 0; i < arr.length; i++) {
             if(arr[i]==target){
-                System.out.println("element found at index "+i);
+                count++;
             }
         }
+        System.out.println("Element frequency "+count);
     }
 }
