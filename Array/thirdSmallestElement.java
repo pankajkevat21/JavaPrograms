d4cf07d73dac726bbef92c3f2b7fedb4f02db445
Pankaj Kevat <pankajkevat21@gmail.com>
2026-06-28 18:17:15 +0530

    add program to find third element in an array

--- Array/thirdSmallestElement.java
+++ Array/thirdSmallestElement.java
@@ -1,4 +1,31 @@
 package Array;
 
 public class thirdSmallestElement {
+    public static void main(String[] args) {
+
+        int[] arr = {12, 5, 8, 1, 20, 3, 9};
+
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+        int third = Integer.MAX_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < first) {
+                third = second;
+                second = first;
+                first = arr[i];
+            } else if (arr[i] < second && arr[i] != first) {
+                third = second;
+                second = arr[i];
+            } else if (arr[i] < third && arr[i] != second && arr[i] != first) {
+                third = arr[i];
+            }
+        }
+
+        if (third == Integer.MAX_VALUE) {
+            System.out.println("Third smallest element does not exist.");
+        } else {
+            System.out.println("Third smallest element = " + third);
+        }
+    }
 }
