aa52b452a2b4411dee1a4c0fb9889bdcb1df54d0
Pankaj Kevat <pankajkevat21@gmail.com>
2026-06-28 19:21:01 +0530

    count unique element in an array and print duplicate element in an array

--- Array/duplicateElement.java
+++ Array/duplicateElement.java
@@ -11,6 +11,7 @@ int count =0;
         for (int i = 0; i < arr.length; i++) {
             for (int j = i+1; j < arr.length; j++) {
                 if(arr[i]==arr[j]){
+                    System.out.print(arr[i]+" ");
                     visited[i]= true;
                     visited[j] =true;
                 }
