11b4ff0e205881cf64732aef478b43674e92430a
Pankaj Kevat <pankajkevat21@gmail.com>
2026-06-30 22:34:58 +0530

    check two array equal or not

--- Array/twoArrEqual.java
+++ Array/twoArrEqual.java
@@ -8,27 +8,16 @@ public class twoArrEqual {
             System.out.println("both arrays are not equal");
             return;
         } boolean isEqual = true;
-
         for (int i = 0; i < arr1.length; i++) {
-
             if (arr1[i] != arr2[i]) {
-
                 isEqual = false;
-
                 break;
-
             }
-
         }
-
         if (isEqual) {
-
             System.out.println("Both arrays are equal");
-
         } else {
-
             System.out.println("Both arrays are not equal");
-
         }
     }
 }
