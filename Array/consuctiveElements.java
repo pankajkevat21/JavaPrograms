4b67e38ec67cde6b543425571c2876b8fe38a46a
Pankaj Kevat <pankajkevat21@gmail.com>
2026-06-30 22:32:41 +0530

    Consuctive element in an array

--- Array/consuctiveElements.java
+++ Array/consuctiveElements.java
@@ -1,4 +1,12 @@
 package Array;
 
 public class consuctiveElements {
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 9, 14, 20};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int difference = arr[i + 1] - arr[i];
+            System.out.println(arr[i + 1] + " - " + arr[i] + " = " + difference);
+        }
+    }
 }
