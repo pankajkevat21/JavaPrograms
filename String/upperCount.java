e007487b2f96e5b39ea900ff5c173136cefca401
Pankaj Kevat <pankajkevat21@gmail.com>
2026-07-04 23:54:05 +0530

    Add uppercase letter counting program

--- String/upperCount.java
+++ String/upperCount.java
@@ -1,4 +1,16 @@
 package String;
 
 public class upperCount {
+    public static void main(String[] args) {
+        String text = "Hello World! Welcome To Java Programming 123";
+
+        int uppercaseCount = 0;
+        for (int i = 0; i < text.length(); i++) {
+             char ch = text.charAt(i);
+             if (ch >= 'A' && ch <= 'Z') {
+                 uppercaseCount++;
+             }
+        }
+        System.out.println("Total uppercase letters: " + uppercaseCount);
+    }
 }
