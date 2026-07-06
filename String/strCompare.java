85bf518b4e65acaeacf86106af3dd38b1283c7a3
Pankaj Kevat <pankajkevat21@gmail.com>
2026-07-06 23:05:23 +0530

    Add string comparison example

---
+++ String/strCompare.java
@@ -0,0 +1,16 @@
+package String;
+
+public class strCompare {
+    public static void main(String[] args) {
+        System.out.println("Ritesh Kevat");
+
+        String str1 = "Hello";
+        String str2 = "Hello";
+
+        if (str1.equals(str2)) {
+            System.out.println("Strings are equal.");
+        } else {
+            System.out.println("Strings are not equal.");
+        }
+    }
+}
