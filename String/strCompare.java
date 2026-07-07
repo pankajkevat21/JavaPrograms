08f2a4ead38975a1e0aa01bf3ad7237cf969ec3d
Pankaj Kevat <pankajkevat21@gmail.com>
2026-07-07 09:36:15 +0530

    compare two strings

--- String/strCompare.java
+++ String/strCompare.java
@@ -2,7 +2,6 @@ package String;
 
 public class strCompare {
     public static void main(String[] args) {
-        System.out.println("Ritesh Kevat");
 
         String str1 = "Hello";
         String str2 = "Hello";
