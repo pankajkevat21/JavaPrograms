5292205ccfd96b76b2ce59716430431fedfa2aaf
Pankaj Kevat <pankajkevat21@gmail.com>
2026-09-10 09:23:59 +0530

    hashmap implementation

--- Interview/Hsmap.java
+++ Interview/Hsmap.java
@@ -1,4 +1,18 @@
 package Interview;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Hsmap {
+    public  static void main(String[] args) {
+       HashMap<Integer,String> map = new HashMap<>();
+        map.put(121,"Pankaj");
+        map.put(122,"Rohit");
+        map.put(123,"Rahul");
+        map.put(124,"Aman");
+//        System.out.println(map);
+        for(Map.Entry<Integer,String>e:map.entrySet()){
+            System.out.print(e.getKey()+ " ");
+            System.out.println(e.getValue()+" ");
+        }
+    }
 }
