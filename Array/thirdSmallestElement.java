7297813e707a47957609a0bc4e7ec0bfd3fb08d1
Pankaj Kevat <pankajkevat21@gmail.com>
2026-09-10 09:15:18 +0530

    Add third smallest element finder

--- Array/thirdSmallestElement.java
+++ Array/thirdSmallestElement.java
@@ -1,14 +1,10 @@
 package Array;
-
 public class thirdSmallestElement {
     public static void main(String[] args) {
-
         int[] arr = {12, 5, 8, 1, 20, 3, 9};
-
         int first = Integer.MAX_VALUE;
         int second = Integer.MAX_VALUE;
         int third = Integer.MAX_VALUE;
-
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] < first) {
                 third = second;
@@ -21,7 +17,6 @@ public class thirdSmallestElement {
                 third = arr[i];
             }
         }
-
         if (third == Integer.MAX_VALUE) {
             System.out.println("Third smallest element does not exist.");
         } else {
