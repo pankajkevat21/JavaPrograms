c6a630015cbff2ec53a0e48470638defe6756418
Pankaj Kevat <pankajkevat21@gmail.com>
2026-09-10 09:24:23 +0530

    Inheritance implementad

--- Interview/Inheritance.java
+++ Interview/Inheritance.java
@@ -1,4 +1,25 @@
 package Interview;
 
-public class Inheritance {
+class Inherit {
+    public void mymethod(int m) {
+
+            System.out.println("The value of n = 13");
+
+    }
+}
+class Inherit2 extends Inherit{
+    public void mymethod(int k){
+
+        super.mymethod(12);
+        System.out.println("The code is inside the base2 class");
+    }
+}
+public class Inheritance extends Inherit2 {
+
+    public static void main(String[] args) {
+
+        Inheritance in = new Inheritance();
+
+        in.mymethod(1);
+    }
 }
