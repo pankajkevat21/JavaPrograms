e6135c1ac00f0e3ff66d4ce1ad1b3f12c74dfb4b
Pankaj Kevat <pankajkevat21@gmail.com>
2026-09-15 21:30:51 +0530

    Count digits

--- String/digitsCount.java
+++ String/digitsCount.java
@@ -1,4 +1,15 @@
 package String;
-
 public class digitsCount {
+    public static void main(String[] args) {
+        String inputString = "abc123xyz4567";
+        int digitCount = 0;
+        for (int i = 0; i < inputString.length(); i++) {
+            char currentCharacter = inputString.charAt(i);
+
+            if (currentCharacter >= '0' && currentCharacter <= '9') {
+                digitCount++;
+            }
+        }
+        System.out.println("Total digits: " + digitCount);
+    }
 }
