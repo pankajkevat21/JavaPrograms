bbe51690259af96d530c5fc4028934a4930db23a
Pankaj Kevat <pankajkevat21@gmail.com>
2026-09-15 21:28:31 +0530

    find a missing number in java

--- LeetCode/missingNumber.java
+++ LeetCode/missingNumber.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class missingNumber {
+    public int miss(int[] nums) {
+        int N =nums.length;
+
+        int missnum = N*(N+1)/2;
+        int arsum =0;
+        for(int sum : nums){
+            arsum += sum;
+        }
+
+        return (missnum-arsum);
+    }
+
+    public static void main(String[] args) {
+        int a[] ={1,2,3,5,6};
+        missingNumber m1= new missingNumber();
+
+        System.out.println(m1.miss(a));
+    }
 }
