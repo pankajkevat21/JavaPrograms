067a3b37ae65497ece221f6eb5cd55557cd4cd44
Pankaj Kevat <pankajkevat21@gmail.com>
2026-09-15 21:26:43 +0530

    Git funcation creation

--- Interview/Git.java
+++ Interview/Git.java
@@ -3,6 +3,6 @@ package Interview;
 public class Git {
     public static void main(String[] args) {
         System.out.println("helsdflo i am pankaj kevat bhum");
-
+        System.out.println("Hello from Git.java");
     }
 }
