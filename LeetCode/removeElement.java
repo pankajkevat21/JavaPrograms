461da4bfd6ae13c1c9a48e090252e6f382aa0184
Pankaj Kevat <pankajkevat21@gmail.com>
2026-09-16 14:48:23 +0530

    Remove elements in the array

--- LeetCode/removeElement.java
+++ LeetCode/removeElement.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class removeElement {
+    public int remove(int[] nums, int val) {
+        int slow = 0;
+        for (int i = 0; i < nums.length; i++) {
+
+            if(nums[i] != val){
+                nums[slow++] = nums[i];
+            }
+        }
+        return slow;
+    }
+
+   public static void main(String[] args) {
+removeElement rm = new removeElement();
+int arr[] ={0,1,2,2,3,0,4,2};
+
+int ans =rm.remove(arr,2);
+       System.out.println(ans);
+    }
 }
