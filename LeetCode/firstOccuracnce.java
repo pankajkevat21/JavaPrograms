4c582f3d42cc43a8c1e1e56525e5152bd2c02ce1
Pankaj Kevat <pankajkevat21@gmail.com>
2026-09-22 16:54:57 +0530

    first occurance

--- LeetCode/firstOccuracnce.java
+++ LeetCode/firstOccuracnce.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+class firstOccurance {
+        public int strStr(String haystack, String needle) {
+            int n = haystack.length(), m = needle.length();
+            if (m == 0) return 0;
 
-public class firstOccuracnce {
-}
+            // 1) Build LPS (longest proper prefix-suffix) array for needle
+            int[] lps = new int[m];
+            for (int i = 1, len = 0; i < m; ) {
+                if (needle.charAt(i) == needle.charAt(len)) {
+                    lps[i++] = ++len;
+                } else if (len > 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i++] = 0;
+                }
+            }
+
+            // 2) Search
+            for (int i = 0, j = 0; i < n; ) {
+                if (haystack.charAt(i) == needle.charAt(j)) {
+                    i++; j++;
+                    if (j == m) return i - j;   // full match
+                } else if (j > 0) {
+                    j = lps[j - 1];             // fall back without re-scanning
+                } else {
+                    i++;
+                }
+            }
+            return -1;
+        }
+        public static void main(String[] args) {
+            firstOccurance fn = new firstOccurance();
+            System.out.println(fn.strStr("sadbutsab","sad"));
+        }
+    }
