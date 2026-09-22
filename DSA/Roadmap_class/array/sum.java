d830185ac15176ab63c5f7e4b96164c0c356e964
Pankaj Kevat <pankajkevat21@gmail.com>
2026-09-22 16:53:35 +0530

    sum of an array

--- DSA/Roadmap_class/array/sum.java
+++ DSA/Roadmap_class/array/sum.java
@@ -1,4 +1,17 @@
 package Roadmap_class.array;
 
 public class sum {
+    public static void main(String[] args) {
+        int arr[] ={12,56,3,61,60};
+        int sum =0;
+        int large =arr[0];
+        for(int summ:arr){
+            if(summ>large){
+                large=summ;
+            }
+            sum+=summ;
+        }
+        System.out.println("the sum of the array:"+sum);
+        System.out.println("the largest element:"+large);
+    }
 }
