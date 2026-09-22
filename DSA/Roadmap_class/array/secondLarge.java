813401ba6d1cac37ff2bfa1042ec32db2617c5be
Pankaj Kevat <pankajkevat21@gmail.com>
2026-09-22 16:53:01 +0530

    second largest element

--- DSA/Roadmap_class/array/secondLarge.java
+++ DSA/Roadmap_class/array/secondLarge.java
@@ -1,4 +1,24 @@
 package Roadmap_class.array;
 
 public class secondLarge {
+    public static void main(String[] args) {
+        int arr[] = {7,14,15,21,19};
+        if(arr.length==0){
+            System.out.println("Array is empty");
+            return;
+        }
+        int second_large =0;
+        int large =arr[0];
+        for(int num:arr){
+            if(large<num){
+                second_large =large;
+                large = num;
+            }
+            if(second_large<num && large!=num){
+                second_large =num;
+            }
+        }
+        System.out.println("Largest:"+large);
+        System.out.println("secondLargest:"+second_large);
+    }
 }
